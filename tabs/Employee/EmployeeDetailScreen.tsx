e2d3cb2ac5e3ceb7deff18c3398941f84a6d6210
Tay Guo Hong <>
2023-12-08 16:19:45 +0800

    Commit new page for employee

---
+++ tabs/Employee/EmployeeDetailScreen.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import {
+    Alert,
+    Modal,
+    StyleSheet,
+    Text,
+    Pressable,
+    View,
+    TouchableWithoutFeedback,
+} from "react-native";
+import { getBaseStyle } from "../../lib/style/GlobalStyle";
+import {
+    ContainerView,
+    ScrollContainerView,
+} from "../../lib/components/ContainerView";
+import { Button } from "../../lib/components/Button";
+
+export const EmployeeDetailScreen = () => {
+    return (
+        <View>
+         <Text>
+            Employee Detail Screen
+         </Text>
+        </View>
+    );
+};
