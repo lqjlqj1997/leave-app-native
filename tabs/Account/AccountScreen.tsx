73b049ced0ddcfd32079b823e5f0f197ac1e051e
EBGLOBALJV\cy.lee <chiyang.rt@rakutentrade.my>
2023-12-08 10:32:30 +0800

    revamp UI

--- tabs/Account/AccountScreen.tsx
+++ tabs/Account/AccountScreen.tsx
@@ -1,15 +1,17 @@
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { SafeAreaView, Text, Image, TextInput } from "react-native";
+import { SafeAreaView, Text, Image, TextInput, Modal, Pressable, TouchableOpacity } from "react-native";
 import { RootStackParamList } from "../../router/Router";
 import { ScrollContainerView } from "../../lib/components/ContainerView";
 import { getBaseStyle } from "../../lib/style/GlobalStyle";
 import { Button } from "../../lib/components/Button";
 import { ContainerView } from "../../lib/components/ContainerView";
 // import { MapPin } from 'lucide-react';
-import { Cake, CheckSquare, Container, Mail, MapPin, User, View } from 'lucide-react-native';
+import { Cake, CheckSquare, Mail, MapPin, User } from 'lucide-react-native';
 import { ScrollView } from "react-native";
 import { Phone } from "lucide-react-native";
+import { View } from "react-native";
+import { useState } from "react";
 
 export function AccountScreen() {
     const baseStyle = getBaseStyle();
@@ -19,8 +21,12 @@ export function AccountScreen() {
         >();
     const image = require("./profile.jpg");
     const userName = "FATIMAH"
+    const handleUpdateButtonPress = () => {
+        // navigation.navigate("")
+    }
+    const [modalVisible, setModalVisible] = useState(false);
     return (
-        <SafeAreaView style={{flex:1}}>
+        <SafeAreaView style={{ flex: 1 }}>
             <ScrollContainerView
                 style={
                     {
@@ -33,51 +39,97 @@ export function AccountScreen() {
             >
 
                 <ContainerView style={{
-                        width: "100%",
-                        borderWidth: 0,
-                          
-                    }}>
+                    width: "100%",
+                    borderWidth: 0,
+
+                }}>
                     <Image source={image} style={{ width: 200, height: 200, borderRadius: 400 / 2 }} />
                     <Text style={{ fontSize: 30 }}>{userName}</Text>
                     <ContainerView style={{
                         width: "100%",
-                        maxWidth:800,
-                        alignItems:"flex-start",
+                        maxWidth: 800,
+                        alignItems: "flex-start",
                     }}>
                         <Text>User Details</Text>
-                        <ContainerView style={{ flexDirection: "row",width: "100%", }}>
+                        <ContainerView style={{ flexDirection: "row", width: "100%", }}>
                             <Mail color={baseStyle.primary} />
-                            <Text style={{flex:1}}>Email</Text>
+                            <Text style={{ flex: 1 }}>Email</Text>
                             {/* <TextInput style={{flex:6, borderBlockColor:"black",borderWidth: 1,}} value="text"></TextInput> */}
-                            
+
                         </ContainerView>
-                        <ContainerView style={{ flexDirection: "row" ,width: "100%",}}>
+                        <ContainerView style={{ flexDirection: "row", width: "100%", }}>
                             <MapPin color={baseStyle.primary} />
-                            <Text style={{flex:6,width: "100%",}}>Address</Text>
+                            <Text style={{ flex: 6, width: "100%", }}>Address</Text>
                         </ContainerView>
-                        <ContainerView style={{ flexDirection: "row" ,width: "100%", }}>
+                        {/* <TouchableOpacity
+                            onPress={() => {
+                                setModalVisible(true);
+                            }}
+                            style={{ flexDirection: "row", width: "100%", }}
+                        >
+                            <ContainerView style={{ flexDirection: "row", width: "100%", backgroundColor: "white" }}>
+                                <MapPin color={baseStyle.primary} />
+                                <Text style={{ flex: 6, width: "100%", }}>Test</Text>
+                            </ContainerView>
+                        </TouchableOpacity> */}
+
+                        <ContainerView style={{ flexDirection: "row", width: "100%", }}>
                             <Cake color={baseStyle.primary} />
-                            <Text style={{flex:6}}>BOD</Text>
+                            <Text style={{ flex: 6 }}>BOD</Text>
                         </ContainerView>
-                        <ContainerView style={{ flexDirection: "row" ,width: "100%"}}>
+                        <ContainerView style={{ flexDirection: "row", width: "100%" }}>
                             <CheckSquare color={baseStyle.primary} />
-                            <Text style={{flex:6}}>Status</Text>
+                            <Text style={{ flex: 6 }}>Status</Text>
                         </ContainerView>
-                        <ContainerView style={{ flexDirection: "row",width: "100%" }}>
+                        <ContainerView style={{ flexDirection: "row", width: "100%" }}>
                             <Phone color={baseStyle.primary} />
-                            <Text style={{flex:6}}>Phone Number</Text>
+                            <Text style={{ flex: 6 }}>Phone Number</Text>
                         </ContainerView>
-                        <ContainerView style={{ flexDirection: "row",width: "100%" }}>
+                        <ContainerView style={{ flexDirection: "row", width: "100%" }}>
                             <User color={baseStyle.primary} />
-                            <Text style={{flex:6}}>Role</Text>
+                            <Text style={{ flex: 6 }}>Role</Text>
                         </ContainerView>
 
                     </ContainerView>
-                        <Button title="Update Profile"
-                        // onPress={handleUpdateButtonPress}
-                        ></Button>
+                    <Button title="Update Profile"
+                        onPress={() => setModalVisible(true)}
+                    ></Button>
                 </ContainerView>
 
+                <View style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    marginTop: 22,
+                }}
+                >
+                    <Modal
+                        transparent={true}
+                        visible={modalVisible}
+                        animationType="fade"
+                        onRequestClose={() => {
+                            setModalVisible(!modalVisible);
+                        }}>
+                        <ContainerView
+                            style={{
+                                width: "100%",
+                                height: "100%",
+                                backgroundColor: baseStyle.overlay,
+                            }}>
+                            <ContainerView style={{
+                                width: "100%",
+                                height: "100%",
+                                minWidth: 350,
+                                maxWidth: 800,
+                                minHeight: 200,
+                                maxHeight: 600,
+                            }}>
+                                <Text style={{ flexDirection: "row" }}> heelo</Text>
+                                <Button title="Close" onPress={() => setModalVisible(false)} />
+                            </ContainerView>
+                        </ContainerView>
+                    </Modal>
+                </View>
             </ScrollContainerView>
         </SafeAreaView>
     );
