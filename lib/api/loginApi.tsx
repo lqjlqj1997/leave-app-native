26518a20f47ef7a81871774c81bfd33616c1cdfe
Desmond Lin <desmond.rt@rakutentrade.my>
2024-01-08 15:09:29 +0800

    Enhanced login form with react hook form

--- lib/api/loginApi.tsx
+++ lib/api/loginApi.tsx
@@ -1,23 +1,32 @@
 import axios from "axios";
+import { z } from "zod";
 
-const handleSubmit = (loginEmail: string, password: string) => {
-        console.log(loginEmail);
-        console.log(password)
-        try {
-            axios.post("http://localhost:8080/employee/login", {
+export const loginApiSchema = z.object({
+    token: z.string(),
+    roleList: z.array(z.object({
+        
+    })),
+});
+
+export const login = (loginEmail: string, password: string) => {
+    console.log(loginEmail);
+    console.log(password);
+    try {
+        axios
+            .post("http://localhost:8080/employee/login", {
                 loginEmail,
                 password,
-            }).then(function (response) {
-                const { token, roleList } = response.data;
+            })
+            .then(function (response) {
+                const { token, roleList } = loginApiSchema.parse(response.data);
                 localStorage.setItem("token", token);
                 localStorage.setItem("role", JSON.stringify(roleList));
-            }
-            );
-        } catch (error) {   
-            console.log("error");
-            return false
-        }
-        return true
+            });
+    } catch (error) {
+        console.log("error");
+        return false;
     }
+    return true;
+};
 
-    //props -> check
+//props -> check
--- tabs/Auth/Login.tsx
+++ tabs/Auth/Login.tsx
@@ -1,17 +1,28 @@
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import axios from "axios";
 import React, { useState } from "react";
-import { Text, TextInput } from "react-native";
-import { RootStackParamList, RootTabParamList } from "../../router/Router";
+import { Text, TextInput, View } from "react-native";
 import { useThemeStore } from "../../global-store/ThemeStore";
 import { Button } from "../../lib/components/Button";
 import { ContainerView } from "../../lib/components/ContainerView";
 import { ThemeSwitch } from "../../lib/components/ThemeToggle";
-import { getBaseStyle } from "../../lib/style/StyleUtil";
+import { getBaseStyle, getDefaultColourStyle } from "../../lib/style/StyleUtil";
+import { RootStackParamList, RootTabParamList } from "../../router/Router";
 import { Layout } from "./Layout";
-import axios from "axios";
-import { LOGIN_URL } from "@env";
 
+import { LOGIN_URL } from "@env";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useMutation } from "@tanstack/react-query";
+import { tw } from "../../lib/util/Tailwind";
+
+const loginSchema = z.object({
+    loginEmail: z.string().email("This is Not Email"),
+    password: z.string(),
+});
+type loginSchemaType = z.infer<typeof loginSchema>;
 
 export function LoginScreen() {
     const navigation =
@@ -24,30 +35,50 @@ export function LoginScreen() {
     const isDark = useThemeStore((state) => state.isDark);
     const baseStyle = getBaseStyle(isDark);
 
-    
-    const [loginEmail, setLoginEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
+    const { defaultFontColor } = getDefaultColourStyle();
 
-    const handleSubmit = (loginEmail: string, password: string) => {
+    const {
+        control,
+        formState: { isLoading, isSubmitted, errors },
+        handleSubmit,
+        setError,
+    } = useForm<loginSchemaType>({ resolver: zodResolver(loginSchema) });
+
+    console.log(errors);
+
+    const onSubmit = async (loginEmail: string, password: string) => {
         // console.log(LOGIN_URL);
         // console.log("loginEmail:" + loginEmail);
         // console.log(password)
-        try {
-            axios.post(LOGIN_URL, {
+
+        const response = await axios
+            .post(LOGIN_URL, {
                 loginEmail,
                 password,
-            }).then(function (response) {
-                const { token, role  } = response.data;
-                localStorage.setItem("token", token);
-                localStorage.setItem("role", role);
-                navigation.navigate("App");
-                console.log("success");
-            }
-            );
-        } catch (error) {
-            console.log("error");
-        }
-    }
+            })
+            .catch((error) => {
+                // console.log(error);
+                let message = error.message;
+                try {
+                    message = error.response.data.message;
+                } catch (e) {
+                    message = error.message;
+                }
+                setError("root", { type: "API", message: message });
+            })
+            .catch((error) => {
+                console.log(error);
+                setError("root", { type: "API", message: error.message });
+            });
+
+        if (!response) return;
+
+        const { token, role } = response.data;
+        localStorage.setItem("token", token);
+        localStorage.setItem("role", role);
+        navigation.navigate("App");
+        console.log("success");
+    };
 
     return (
         <Layout>
@@ -74,60 +105,107 @@ export function LoginScreen() {
                     Login
                 </Text>
 
-                <TextInput
-                    style={{
-                        // flex: 1,
-                        display: "flex",
-                        height: baseStyle.space.p10,
-                        width: "100%",
-                        borderWidth: 1,
-                        borderColor: baseStyle.color.input,
-                        borderRadius: baseStyle.rounded.md,
-                        backgroundColor: baseStyle.color.background,
-                        paddingHorizontal: baseStyle.space.p3,
-                        paddingVertical: baseStyle.space.p2,
-                        fontSize: baseStyle.fontSize.base,
-                        shadowColor: baseStyle.color.background,
-                    }}
-                    placeholder="Email"
-                    placeholderTextColor={baseStyle.color.mutedForeground}
-                    keyboardType="email-address"
-                    onChangeText={setLoginEmail}
+                <Controller
+                    name="loginEmail"
+                    control={control}
+                    render={(prop) => (
+                        <View
+                            style={{
+                                width: "100%",
+                            }}>
+                            <TextInput
+                                style={{
+                                    // flex: 1,
+                                    display: "flex",
+                                    height: baseStyle.space.p10,
+                                    width: "100%",
+                                    borderWidth: 1,
+                                    borderColor: baseStyle.color.input,
+                                    borderRadius: baseStyle.rounded.md,
+                                    backgroundColor: baseStyle.color.background,
+                                    paddingHorizontal: baseStyle.space.p3,
+                                    paddingVertical: baseStyle.space.p2,
+                                    fontSize: baseStyle.fontSize.base,
+                                    shadowColor: baseStyle.color.background,
+                                }}
+                                placeholder="Email"
+                                placeholderTextColor={
+                                    baseStyle.color.mutedForeground
+                                }
+                                keyboardType="email-address"
+                                onChangeText={prop.field.onChange}
+                                onBlur={prop.field.onBlur}
+                                value={prop.field.value}
+                            />
+                            {prop.fieldState.error && (
+                                <Text style={[tw`text-red-500 pl-2`]}>
+                                    {prop.fieldState.error.message}
+                                </Text>
+                            )}
+                        </View>
+                    )}
                 />
 
-                <TextInput
-                    secureTextEntry={true}
-                    style={{
-                        // flex: 1,
-                        display: "flex",
-                        height: baseStyle.space.p10,
-                        width: "100%",
-                        borderWidth: 1,
-                        borderColor: baseStyle.color.input,
-                        borderRadius: baseStyle.rounded.md,
-                        backgroundColor: baseStyle.color.background,
-                        paddingHorizontal: baseStyle.space.p3,
-                        paddingVertical: baseStyle.space.p2,
-                        fontSize: baseStyle.fontSize.base,
-                        shadowColor: baseStyle.color.background,
-                    }}
-                    placeholder="Password"
-                    placeholderTextColor={baseStyle.color.mutedForeground}
-                    onChangeText={setPassword}
+                <Controller
+                    name="password"
+                    control={control}
+                    render={(prop) => (
+                        <View
+                            style={{
+                                width: "100%",
+                            }}>
+                            <TextInput
+                                secureTextEntry={true}
+                                style={{
+                                    // flex: 1,
+                                    display: "flex",
+                                    height: baseStyle.space.p10,
+                                    width: "100%",
+                                    borderWidth: 1,
+                                    borderColor: baseStyle.color.input,
+                                    borderRadius: baseStyle.rounded.md,
+                                    backgroundColor: baseStyle.color.background,
+                                    paddingHorizontal: baseStyle.space.p3,
+                                    paddingVertical: baseStyle.space.p2,
+                                    fontSize: baseStyle.fontSize.base,
+                                    shadowColor: baseStyle.color.background,
+                                }}
+                                placeholder="Password"
+                                placeholderTextColor={
+                                    baseStyle.color.mutedForeground
+                                }
+                                onChangeText={prop.field.onChange}
+                                onBlur={prop.field.onBlur}
+                                value={prop.field.value}
+                            />
+                            {prop.fieldState.error && (
+                                <Text style={[tw`text-red-500 pl-2`]}>
+                                    {prop.fieldState.error.message}
+                                </Text>
+                            )}
+                        </View>
+                    )}
                 />
+
+                {errors.root && (
+                    <ContainerView style={[tw`border-red-500 bg-red-100`]}>
+                        <Text style={[tw`text-red-500`]}>
+                            {errors.root.message}
+                        </Text>
+                    </ContainerView>
+                )}
+
                 <Button
                     title="Login"
-                    // onPress={() => navigation.navigate("App")}
-                    onPress={() => handleSubmit(loginEmail, password)}
-                ></Button>
+                    onPress={handleSubmit((data) =>
+                        onSubmit(data.loginEmail, data.password)
+                    )}
+                    disabled={isSubmitted}></Button>
                 <Button
                     title="Home"
-                    onPress={() => navigation.navigate("Home")}
-                    // onPress={() => handleSubmit(loginEmail, password)}
-                ></Button>
+                    onPress={() => navigation.navigate("Home")}></Button>
                 <ThemeSwitch />
             </ContainerView>
-            
         </Layout>
     );
 }
